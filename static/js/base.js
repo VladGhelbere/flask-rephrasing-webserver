1becd2e84e7b7a9010cf8ed0bd7a9bc59de6ce49
Vlad Ghelbere <vladut.ghelbere98@e-uvt.ro>
2021-06-19 02:39:59 +0300

    Revert "first stable build"
    This reverts commit 93696e7a67c9e13e4574c11cecb6401554aa256b.

--- static/js/base.js
+++
@@ -1,8 +0,0 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("navbarNav");
-var btns = header.getElementsByClassName("nav-link");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-	  this.className += " active";
-	  });
-}
